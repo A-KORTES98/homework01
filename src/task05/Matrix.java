ece7b5e43fd2d33b3a6d8f666ca0d4376c600768
A-KORTES98 <toni.lesnikov.98@mail.ru>
2017-09-25 20:39:33 +0300

    improves decision's task5

--- src/task05/Matrix.java
+++ src/task05/Matrix.java
@@ -15,14 +15,12 @@ public class Matrix {
         for (int i = 0; i < n; i++) {
             matrix[i][i] = 1;
             matrix[i][n - i - 1] = 1;
-        }
-
-        for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
                 System.out.print(matrix[i][j]);
             }
             System.out.println();
         }
+
     }
 
 }
